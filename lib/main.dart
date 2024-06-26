c6b347fc5ebc4080d3a1296671854575f7aac946
paulsontiti <paulsontiti@gmail.com>
2024-06-26 13:13:58 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:voice_assistant/home_page.dart';
 import 'package:voice_assistant/pallete.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-void main() async{
-  await dotenv.load(fileName: ".env");
+void main() {
   runApp(const MyApp());
 }
 
